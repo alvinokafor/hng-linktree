26f05d5c137ad57b9b034542366a48b89daaca31
Alvin Okafor <alvin2k99@gmail.com>
2022-10-20 22:30:37 +0100

    ui: button component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <section className="btn-section">
+        <Button />
+      </section>
+      
     </div>
   );
 }
